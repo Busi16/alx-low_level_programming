67091aaca38d5d6c27d1a29c01863ed46792fe7d
Busi16 <busisiwemjeza@gmail.com>
2022-12-14 04:49:31 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,24 @@
+#include "main.h"
+
 /**
+ * main - entry point
  *
- *  * main - check the code
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
 
-	    print_alphabet();
+	    _putchar('_');
+	    _putchar('p');
+	    _putchar('u');
+	    _putchar('t');
+	    _putchar('c');
+	    _putchar('h');
+	    _putchar('a');
+	    _putchar('r');
+	    _putchar('\n');
 
 	        return (0);
 
