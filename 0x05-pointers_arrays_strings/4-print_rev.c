4770d96365c778187888d4dd92499ad3e2196ab0
Busi16 <busisiwemjeza@gmail.com>
2022-12-20 14:13:32 -0800

    Update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,2 +1,27 @@
-will figure putchar out later
+#include <stdio.h>
 
+/**
+ * rev_string - reverse a string.
+ *
+ * @s: the string to be reversed.
+ */
+	
+void print_rev(char *s)
+	
+{	
+		int i = 0;
+	
+		int length;
+	
+	
+	
+		for (length = 0; s[length] != '\0'; length++)
+	
+		for (i = length - 1; i >= 0; i--)
+	
+		{
+			_putchar(s[i]);
+		}
+		_putchar('\n');
+		
+}
