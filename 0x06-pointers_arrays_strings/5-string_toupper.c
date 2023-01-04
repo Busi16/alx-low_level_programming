07080a51c98c9a8d421f188914eae62a19d6cf5e
Busi16 <busisiwemjeza@gmail.com>
2023-01-04 06:49:07 -0800

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
- * I will edit later
+ * string_toupper- changes all lowercase letters o
+ * f a string to uppercase
+ * @str: string to changed
+ * Return: pointer to the string
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 	int i = 0;
 
